757d584017ef673b839f71a8076cb13773b23128
anikaaurony <aurony111@gmail.com>
2023-05-19 23:20:50 +0600

    banner commit

--- src/component/Shared/Header/Header.jsx
+++ src/component/Shared/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-[purple] text-white">
+            <div className="navbar bg-[#a21caf] text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
